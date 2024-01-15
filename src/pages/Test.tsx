bf684015a852c021ee2b2296d96b0637e5e63628
Johnny Koo <johnnykoo@Johnnys-Mac-mini.local>
2024-01-15 13:45:59 +0900

    update useSearchParams example

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 export interface ITestPageProps {}
 
 const TestPage: React.FunctionComponent<ITestPageProps> = (props) => {
     const [searchParams, setSearchParams] = useSearchParams();
+    const [brand, setBrand] = useState('');
+    const car = searchParams.get('car');
+    const all = searchParams.toString();
 
-    const food = searchParams.get('food');
-    const dog = searchParams.get('dog');
+    console.log('searchParams', searchParams);
+
+    const handleCarBrandInput = (e: any) => {
+        setBrand(e.target.value);
+    };
+
+    const setCarBrand = () => {
+        console.log('set!');
+        console.log('all strings before action', searchParams.toString());
+        // create a new key & value or update existing key using .set
+        searchParams.set('car', brand);
+        console.log('all strings after action', searchParams.toString());
+
+        // now use setSearchParams to update the latest url you want
+        setSearchParams(searchParams);
+    };
+
+    const appendCarBrand = () => {
+        console.log('append!');
+        console.log('all strings before action', searchParams.toString());
+
+        // append extra key & value using .append
+        searchParams.append('car', brand);
+        console.log('all strings after action', searchParams.toString());
+
+        // now use setSearchParams to update the latest url you want
+        setSearchParams(searchParams);
+    };
 
     return (
         <div>
             <p>This is the test page.</p>
-            {food && <p>Favorite food is: {food}</p>}
-            {dog && <p>My dogs name is: {dog}</p>}
-            <button onClick={() => setSearchParams({ food: 'chicken', dog: 'kyle' })}>Change up the Params!</button>
+            {car && <p>Favorite Car Brand is: {car}</p>}
+            {all && <p>all car params in string: {all}</p>}
+            <input style={{ display: 'block', margin: '1rem' }} onChange={handleCarBrandInput} />
+            <button style={{ display: 'block', margin: '1rem' }} onClick={setCarBrand}>
+                reset and update car brand params
+            </button>
+            <button style={{ display: 'block', margin: '1rem' }} onClick={appendCarBrand}>
+                Append car brand params
+            </button>
         </div>
     );
 };
